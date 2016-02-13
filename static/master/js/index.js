9f74f80687fa8b7bfff387119a87ccde2a6aa554
Suriya Kandaswamy <sardines007@gmail.com>
2016-02-12 23:52:43 -0500

    map updates

--- static/master/js/index.js
+++ static/master/js/index.js
@@ -23,7 +23,24 @@ $(document).ready(function() {
   $('#delConfModal').on('show', function() {
  var id=$(this).data('id');document.write(id);
 });
+                  
+  $('.list-group-item').on('click', function() {
+                           $.getJSON( "/package", function( data ) {
+                                     var items = [];
+                                     var lat = 0;
+                                     var long = 0;
+                                     $.each( data, function( key, val ) {
+                                            if(key=='lat') lat=val;
+                                            if(key=='lon') lon=val;
+                                            });
+                                     
+                                     $(".map").html('<iframe class="hfill vfill bare" frameborder="0" src="https://maps.google.com/maps?q='+lat+','+long+'&hl=es;z=14&amp;output=embed"></iframe>');
+                                     }
+                                     });
+  }
 });
+
+
 
 /*
 Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker
