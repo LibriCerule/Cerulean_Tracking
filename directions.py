cd453087e5032ec7ce1fb9815e600d383f88f574
Kent Ma <tnekent@gmail.com>
2016-02-12 20:54:27 -0500

    Joel is a loser

--- directions.py
+++ directions.py
@@ -7,16 +7,17 @@ class Directions(object):
     """
     def __init__(self, filename):
         with open(filename, 'r') as f:
-            self.api_key = f.read()
+            self.api_key = f.read().strip()
             f.close()
         self.gmaps = googlemaps.Client(self.api_key)
         pass
 
     def getData(self, orig, dest):
-        directions = self.gmaps.directions(orig, dest)
+        distanceData = self.gmaps.distance_matrix(orig, dest)
 
-        distance = (directions[0]['legs'][0]['distance']['text']) #value is meter, text is formatted with mile
-        time = (directions[0]['legs'][0]['duration']['text']) #value is seconds, text is formatted
+        time = (distanceData['rows'][0]['elements'][0]['duration']['text'])
+        distance = (distanceData['rows'][0]['elements'][0]['distance']['text'])
+
         output = {distance, time}
-        return output
 
+        return output
